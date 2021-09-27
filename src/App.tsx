afbd0e052b49af2c706d55bb411d0808c97fe4a6
DNS320 <f.thurnheer7@gmail.com>
2021-09-27 14:44:13 +0200

    add toolbar and speeddial

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
 import { useEffect, useState } from 'react'
 import ConnectionOverview from './components/ConnectionOverview'
 import DeviceOverview from './components/DeviceOverview'
@@ -5,6 +8,10 @@ import { Connection } from './models/Connection'
 import { Device } from './models/Device'
 import { getConnections } from './services/ConnectionService'
 import getDevices from './services/DeviceService'
+import logo from './static/WEmulate_logo.png'
+import { version } from './../package.json'
+import SpeedDial from '@mui/material/SpeedDial'
+import AddIcon from '@mui/icons-material/Add'
 
 const App: React.FC = () => {
   const [device, setDevice] = useState<Device>(new Device([], []))
@@ -17,8 +24,23 @@ const App: React.FC = () => {
 
   return (
     <div>
+      <AppBar position="static">
+        <Toolbar>
+          <img alt="logo" width={88} style={{ marginRight: 24 }} src={logo} />
+          <Typography variant="h5" sx={{ flexGrow: 1 }}>
+            WEmulate
+          </Typography>
+          <Typography>{version}</Typography>
+        </Toolbar>
+      </AppBar>
       <DeviceOverview device={device} />
       <ConnectionOverview connections={connections} />
+      <SpeedDial
+        ariaLabel="SpeedDial basic example"
+        sx={{ position: 'fixed', bottom: 16, right: 16 }}
+        icon={<AddIcon />}
+        onClick={() => alert('test')}
+      ></SpeedDial>
     </div>
   )
 }
